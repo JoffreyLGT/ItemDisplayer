e58277bb55cfc3a879305908098f86fc57d9dbdb
Joffrey LAGUT <joffrey.lagut@gmail.com>
2017-03-24 19:50:23 +0100

    Feature: Unit tests for FetchDataFromServer
    - Add a method to convert the downloaded JSON into a list of photos.
    - Add powermock mockito dependencies to mock classes and static methods in unit tests.
    - Add the unit tests of FetchDataFromServer.java.
    - Delete UnitTestExample.java because it is not needed anymore.

--- app/src/main/java/fr/joffreylagut/itemdisplayer/utilities/FetchDataFromServer.java
+++ app/src/main/java/fr/joffreylagut/itemdisplayer/utilities/FetchDataFromServer.java
@@ -23,7 +23,7 @@ import fr.joffreylagut.itemdisplayer.models.Photo;
  * The download is made using Volley.
  *
  * @author Joffrey LAGUT
- * @version 1.0 2017-03-24
+ * @version 1.1 2017-03-26
  */
 
 public abstract class FetchDataFromServer {
@@ -33,6 +33,10 @@ public abstract class FetchDataFromServer {
 
     // URL of the files to fetch
     private static final String ENDPOINT = "http://jsonplaceholder.typicode.com/photos";
+
+    // Variables
+    private static RequestQueue requestQueue;
+
     /**
      * Response ErrorListener triggered when the app is unable to download the JSON from server.
      * It display the error in the logs.
@@ -41,11 +45,9 @@ public abstract class FetchDataFromServer {
         @Override
         public void onErrorResponse(VolleyError error) {
             Log.e(TAG, error.toString());
-        }
+    }
     };
-    // Variables
-    private static RequestQueue requestQueue;
-    private static Gson gson;
+
     /**
      * Response listener triggered when the JSON is loaded from the server.
      * It create a lit of Photo using GSON and display their information in the logs.
@@ -53,10 +55,10 @@ public abstract class FetchDataFromServer {
     private static final Response.Listener<String> onPhotosLoaded = new Response.Listener<String>() {
         @Override
         public void onResponse(String response) {
-            List<Photo> photos = Arrays.asList(gson.fromJson(response, Photo[].class));
+            // We have to convert the response into Photo objects.
+            List<Photo> photos = convertJSONResponseInPhoto(response);
 
-            Log.i(TAG, photos.size() + " photos loaded.");
-
+            // For information, we display all the photos information in the log.
             for (Photo photo : photos) {
                 Log.i(TAG, photo.getId() + " | " + photo.getTitle()
                         + " | " + photo.getUrl() + " | " + photo.getThumbnailUrl());
@@ -71,10 +73,6 @@ public abstract class FetchDataFromServer {
      */
     public static void updateInformation(Context context) {
         requestQueue = Volley.newRequestQueue(context);
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gson = gsonBuilder.create();
-
         fetchPhotos();
 
     }
@@ -86,5 +84,22 @@ public abstract class FetchDataFromServer {
         StringRequest request = new StringRequest(Request.Method.GET, ENDPOINT,
                 onPhotosLoaded, onPhotosError);
         requestQueue.add(request);
+    }
+
+    /**
+     * This function convert the JSON string into a List<Photo>
+     *
+     * @param json String version of the json file to convert into objects.
+     * @return the list of photos included in the JSON.
+     */
+    static List<Photo> convertJSONResponseInPhoto(String json) {
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        List<Photo> photos = Arrays.asList(gson.fromJson(json, Photo[].class));
+        Log.i(TAG, photos.size() + " photos loaded.");
+
+        return photos;
     }
 }
---
+++ app/src/test/java/fr/joffreylagut/itemdisplayer/utilities/FetchDataFromServerUnitTest.java
@@ -0,0 +1,46 @@
+package fr.joffreylagut.itemdisplayer.utilities;
+
+import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.List;
+
+import fr.joffreylagut.itemdisplayer.models.Photo;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * FetchDataFromServerUnitTest.java
+ * Purpose: Handle the unit tests of FetchDataFromServer.java
+ *
+ * @author Joffrey LAGUT
+ * @version 1.0 2017-03-26
+ */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Log.class})
+public class FetchDataFromServerUnitTest {
+
+    @Test
+    public void convertJSONResponseInPhoto() throws Exception {
+        // Mock
+        PowerMockito.mockStatic(Log.class);
+
+        // Preparation
+        int amountOfPhotoInJSON = 6;
+        String jsonExample = "[{\"albumId\":1,\"id\":1,\"title\":\"accusamus beatae ad facilis cum similique qui sunt\",\"url\":\"http://placehold.it/600/92c952\",\"thumbnailUrl\":\"http://placehold.it/150/92c952\"},{\"albumId\":1,\"id\":2,\"title\":\"reprehenderit est deserunt velit ipsam\",\"url\":\"http://placehold.it/600/771796\",\"thumbnailUrl\":\"http://placehold.it/150/771796\"},{\"albumId\":1,\"id\":3,\"title\":\"officia porro iure quia iusto qui ipsa ut modi\",\"url\":\"http://placehold.it/600/24f355\",\"thumbnailUrl\":\"http://placehold.it/150/24f355\"},{\"albumId\":1,\"id\":4,\"title\":\"culpa odio esse rerum omnis laboriosam voluptate repudiandae\",\"url\":\"http://placehold.it/600/d32776\",\"thumbnailUrl\":\"http://placehold.it/150/d32776\"},{\"albumId\":1,\"id\":5,\"title\":\"natus nisi omnis corporis facere molestiae rerum in\",\"url\":\"http://placehold.it/600/f66b97\",\"thumbnailUrl\":\"http://placehold.it/150/f66b97\"},{\"albumId\":1,\"id\":6,\"title\":\"accusamus ea aliquid et amet sequi nemo\",\"url\":\"http://placehold.it/600/56a8c2\",\"thumbnailUrl\":\"http://placehold.it/150/56a8c2\"}]";
+
+        // Run
+        List<Photo> photosFromServer = FetchDataFromServer.convertJSONResponseInPhoto(jsonExample);
+
+        // Result
+        int amountOfPhotosLoaded = photosFromServer.size();
+        assertEquals(amountOfPhotoInJSON, amountOfPhotosLoaded);
+
+    }
+}
